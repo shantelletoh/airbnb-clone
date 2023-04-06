35635939313b06e9e8ccccfcb437aa7dd8fcab46
shantelletoh <shantelleschool@gmail.com>
2023-04-06 15:52:13 -0700

    connect client to server with cors, update RegisterPage form, test RegisterPage axios

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser(e) {
+    e.preventDefault(); // prevent auto page reload
+    axios.post("/register", {
+      // data to send
+      name,
+      email,
+      password,
+    });
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         {/* grow means item grows to fill any available space*/}
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder={"your@gmail.com"} />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder={"your@gmail.com"}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member?{" "}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const cors = require("cors"); // to connect client and server
 const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
 
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.listen(5000);
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({ name, email, password });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
