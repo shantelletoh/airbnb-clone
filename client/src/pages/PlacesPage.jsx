3fd32490db7cd5080805739eee9fb69fdeaea3b2
shantelletoh <shantelleschool@gmail.com>
2023-04-07 16:11:38 -0700

    PlacesPage code refactoring

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
 import axios from "axios";
+import PhotosUploader from "../PhotosUploader";
 
 export default function PlacesPage() {
   const { action } = useParams();
   const [title, setTitle] = useState("");
   const [address, setAddress] = useState("");
   const [addedPhotos, setAddedPhotos] = useState([]);
-  const [photoLink, setPhotoLink] = useState("");
   const [description, setDescription] = useState("");
   const [perks, setPerks] = useState([]);
   const [extraInfo, setExtraInfo] = useState("");
@@ -31,39 +31,6 @@ export default function PlacesPage() {
         {inputDescription(description)}
       </>
     );
-  }
-
-  async function addPhotoByLink(e) {
-    e.preventDefault();
-    const { data: filename } = await axios.post("/upload-by-link", {
-      link: photoLink,
-    });
-    setAddedPhotos((prev) => {
-      return [...prev, filename];
-    });
-    setPhotoLink("");
-  }
-
-  function uploadPhoto(e) {
-    const files = e.target.files; // target is the input
-    // console.log({ files });
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-    axios
-      .post("/upload", data, {
-        headers: {
-          "Content-type": "multipart/form-data",
-        },
-      })
-      .then((response) => {
-        const { data: filenames } = response;
-        // console.log(filename);
-        setAddedPhotos((prev) => {
-          return [...prev, ...filenames];
-        });
-      });
   }
 
   return (
@@ -116,55 +83,10 @@ export default function PlacesPage() {
             />
 
             {preInput("Photos", "more = better")}
-            <div className="flex gap-2">
-              <input
-                type="text"
-                value={photoLink}
-                onChange={(e) => setPhotoLink(e.target.value)}
-                placeholder={"Add using a link"}
-              />
-              <button
-                onClick={addPhotoByLink}
-                className="bg-gray-200 px-4 rounded-2xl"
-              >
-                Add&nbsp;photo
-              </button>
-            </div>
-
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              {addedPhotos.length > 0 &&
-                addedPhotos.map((link) => (
-                  <div className="h-32 flex">
-                    <img
-                      className="rounded-2xl w-full object-cover"
-                      src={"http://localhost:5000/uploads/" + link}
-                    />
-                  </div>
-                ))}
-              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input
-                  type="file"
-                  multiple
-                  className="hidden"
-                  onChange={uploadPhoto}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
+            <PhotosUploader
+              addedPhotos={addedPhotos}
+              onChange={setAddedPhotos}
+            />
 
             {preInput("Description", "description of the place")}
             <textarea
