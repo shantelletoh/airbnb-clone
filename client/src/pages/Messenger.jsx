9f3c8e2d6fd63f850c652698a679f7496e7bde2d
shantelletoh <shantelleschool@gmail.com>
2023-04-14 13:30:58 -0700

    add blue bar to left of selected user

--- client/src/pages/Messenger.jsx
+++ client/src/pages/Messenger.jsx
@@ -66,12 +66,19 @@ export default function Messenger() {
               console.log(userId);
             }}
             className={
-              "border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer " +
+              "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
               (userId === selectedUserId ? "bg-blue-50" : "")
             }
           >
-            <Avatar username={onlinePeople[userId]} userId={userId} />
-            <span className="text-gray-800">{onlinePeople[userId]}</span>
+            {/* blue bar on left of selected user*/}
+            {userId === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            {/* avatar and username */}
+            <div className="flex gap-2 py-2 pl-4 items-center">
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span className="text-gray-800">{onlinePeople[userId]}</span>
+            </div>
           </div>
         ))}
       </div>
