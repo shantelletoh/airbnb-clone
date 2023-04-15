a47720c7b78ec9a368d70a6beb2f5352abc212ae
shantelletoh <shantelleschool@gmail.com>
2023-04-14 23:48:34 -0700

    style sender and receiver messages

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -110,14 +110,23 @@ export default function Messages() {
 
           {/* display all messages */}
           {!!selectedUserId && (
-            <div>
+            <div className="overflow-y-scroll">
               {messagesWithoutDuplicates.map((message) => (
-                <div>
-                  sender: {message.sender}
-                  <br />
-                  my id: {user._id}
-                  <br />
-                  {message.text}
+                <div
+                  className={
+                    message.sender === user._id ? "text-right" : "text-left"
+                  }
+                >
+                  <div
+                    className={
+                      "text-left inline-block p-2 my-2 rounded-sm text-md " +
+                      (message.sender === user._id
+                        ? "bg-blue-500 text-white"
+                        : "bg-white text-gray-500")
+                    }
+                  >
+                    {message.text}
+                  </div>
                 </div>
               ))}
             </div>
