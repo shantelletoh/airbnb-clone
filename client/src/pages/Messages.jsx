fc1a7f281d99d0790b32e10ebca80b8d81d6093d
shantelletoh <shantelleschool@gmail.com>
2023-04-15 01:49:52 -0700

    implement upload attachment basic ui

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -203,6 +203,25 @@ export default function Messages() {
               placeholder="Type your message here"
               className="bg-white flex-grow border rounded-sm p-2"
             />
+
+            {/* upload attachment */}
+            <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
+              <input type="file" className="hidden" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+
+            {/* send message */}
             <button
               type="submit"
               className="bg-blue-500 p-2 text-white rounded-sm"
