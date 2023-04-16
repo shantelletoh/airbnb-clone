bdae1761986b53d13dc65ce3a2a62da984c09b04
shantelletoh <shantelleschool@gmail.com>
2023-04-16 01:50:42 -0700

    implement photo slider and view full image

--- client/src/PlaceGallery.jsx
+++ client/src/PlaceGallery.jsx
@@ -1,98 +1,189 @@
-import {useState} from "react";
-export default function PlaceGallery({place}) {
-  const [showAllPhotos, setShowAllPhotos] = useState(false);
+import { useCallback, useEffect, useRef, useState } from "react";
+// import SimpleImageSlider from "react-simple-image-slider";
 
-  if (showAllPhotos) {
-    return (
-      <div className="absolute inset-0 fixed bg-black text-white min-h-screen">
-        {/* position absolute enables scrolling to view all photos, inset-0 means stretch to top-left-bottom-right*/}
-        <div className="bg-black p-8 grid gap-4">
-          <div>
-            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
-            <button
-              onClick={() => setShowAllPhotos(false)}
-              className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Close photos
-            </button>
-          </div>
-          {place?.photos?.length > 0 &&
-            place.photos.map((photo) => (
-              <div>
-                <img src={"http://localhost:5000/uploads/" + photo} alt="" />
-              </div>
-            ))}
-        </div>
-      </div>
-    );
+const slideStyles = {
+  width: "100%",
+  height: "100%",
+  maxWidth: "700px",
+  maxHeight: "500px",
+  borderRadius: "10px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const rightArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  right: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const leftArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  left: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const slidesContainerStyles = {
+  display: "flex",
+  height: "100%",
+  transition: "transform ease-out 0.3s",
+};
+
+const slidesContainerOverflowStyles = {
+  overflow: "hidden",
+  height: "100%",
+};
+
+export default function PlaceGallery({ place }) {
+  const slides = place.photos.map((photo) => ({
+    url: "http://localhost:5000/uploads/" + photo,
+  }));
+
+  // const timerRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showFullImage, setShowFullImage] = useState(false);
+  const [fullImageIndex, setFullImageIndex] = useState(-1);
+
+  function closeModal() {
+    setShowFullImage(false);
   }
 
+  function openModal() {
+    setShowFullImage(true);
+  }
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = useCallback(() => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }, [currentIndex, slides]);
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const getSlideStylesWithBackground = (slideIndex) => ({
+    ...slideStyles,
+    backgroundImage: `url(${slides[slideIndex].url})`,
+    width: `700px`,
+  });
+  const getSlidesContainerStylesWithWidth = () => ({
+    ...slidesContainerStyles,
+    width: 700 * slides.length,
+    transform: `translateX(${-(currentIndex * 700)}px)`,
+  });
+
+  // useEffect(() => {
+  //   if (timerRef.current) {
+  //     clearTimeout(timerRef.current);
+  //   }
+  //   timerRef.current = setTimeout(() => {
+  //     goToNext();
+  //   }, 2000);
+
+  //   return () => clearTimeout(timerRef.current);
+  // }, [goToNext]);
+
   return (
-    <div className="relative">
-      <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-        <div>
-          {place.photos?.[0] && (
-            <div>
-              <img
-                onClick={() => setShowAllPhotos(true)}
-                className="aspect-square cursor-pointer object-cover"
-                src={"http://localhost:5000/uploads/" + place.photos[0]}
-                alt=""
-              />
+    <div className="flex">
+      <div style={{ width: "700px", height: "500px", margin: "0 auto" }}>
+        <div className="h-full relative">
+          <div>
+            <div onClick={goToPrevious} style={leftArrowStyles}>
+              ❰
             </div>
-          )}
-        </div>
-        <div className="grid">
-          {place.photos?.[1] && (
-            <img
-              onClick={() => setShowAllPhotos(true)}
-              className="aspect-square cursor-pointer object-cover"
-              src={"http://localhost:5000/uploads/" + place.photos[1]}
-              alt=""
-            />
-          )}
-          <div className="overflow-hidden">
-            {place.photos?.[2] && (
-              <img
-                onClick={() => setShowAllPhotos(true)}
-                className="aspect-square cursor-pointer object-cover relative top-2"
-                src={"http://localhost:5000/uploads/" + place.photos[2]}
-                alt=""
-              />
-            )}
+            <div onClick={goToNext} style={rightArrowStyles}>
+              ❱
+            </div>
+          </div>
+          <div style={slidesContainerOverflowStyles}>
+            <div style={getSlidesContainerStylesWithWidth()}>
+              {slides.map((_, slideIndex) => (
+                <div
+                  key={slideIndex}
+                  style={getSlideStylesWithBackground(slideIndex)}
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setShowFullImage(!showFullImage);
+                    setFullImageIndex(slideIndex);
+                  }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="justify-center flex">
+            {slides.map((slide, slideIndex) => (
+              <div key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+                {currentIndex === slideIndex ? (
+                  <div className="mt-2 mr-2 cursor-pointer w-3 h-3 rounded-full border border-gray-400"></div>
+                ) : (
+                  <div className="mt-2 mr-2 cursor-pointer w-3 h-3 bg-gray-400 rounded-full border border-gray-400"></div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <button
-        onClick={() => setShowAllPhotos(true)}
-        className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-        Show more photos
-      </button>
+
+      <div>
+        {showFullImage ? (
+          <div
+            className="fixed top-0 bottom-0 left-0 right-0 bg-gray-600 bg-opacity-40 flex justify-center items-center z-20"
+            onClick={() => {
+              // close modal when outside of modal is clicked
+              setShowFullImage(false);
+            }}
+          >
+            <div
+              className="bg-white p-5"
+              onClick={(e) => {
+                // do not close modal if anything inside modal content is clicked
+                e.stopPropagation();
+              }}
+            >
+              <button
+                onClick={() => {
+                  setShowFullImage(false);
+                }}
+              >
+                Close
+              </button>
+              <img
+                src={
+                  "http://localhost:5000/uploads/" +
+                  place.photos[fullImageIndex]
+                }
+                alt=""
+              />
+              {/* <h1>Look! I'm inside the modal!</h1> */}
+              {/* {children} */}
+            </div>
+          </div>
+        ) : null}
+      </div>
+      {/* <SimpleImageSlider
+        style={{ marginTop: "50px" }}
+        width={896}
+        height={504}
+        images={place.photos.map((photo) => ({
+          url: "http://localhost:5000/uploads/" + photo,
+        }))}
+        showBullets={true}
+        showNavs={true}
+      /> */}
     </div>
   );
 }
