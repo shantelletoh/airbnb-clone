5ee6b8572c845670f9857e443cfde1b3759acc6b
shantelletoh <shantelleschool@gmail.com>
2023-04-20 22:04:35 -0700

    implement search places

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="flex justify-between">
       {/* flex puts items on same line. justify-bet gives as much space between items as poss */}
-      <Link to={'/'} className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-1">
         <svg // https://heroicons.com/
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -32,23 +32,6 @@ export default function Header() {
         <div>Any week</div>
         <div className="border-l border-gray-300"></div>
         <div>Add guests</div>
-        <button className="bg-primary text-white p-1 rounded-full">
-          {/* "primary" color is set up as own custom color in tailwind file */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-        </button>
       </div>
       <Link
         to={user ? "/account" : "/login"}
