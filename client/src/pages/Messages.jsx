689ceb5336bd50aed8738caf420b43d833cd4eba
shantelletoh <shantelleschool@gmail.com>
2023-04-24 21:33:53 -0700

    style messenger

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -145,33 +145,35 @@ export default function Messages() {
   }, [selectedUserId]);
 
   return (
-    <div className="flex h-screen">
-      <div className="bg-white w-1/3">
-        <Logo />
+    <div className="flex h-[85vh] mt-5">
+      <div className="bg-white w-1/3 flex flex-col">
+        <div className="flex-grow">
+          <Logo />
 
-        {/* display online contacts */}
-        {Object.keys(onlinePeopleExclOurUser).map((userId) => (
-          <Contact
-            key={userId}
-            id={userId}
-            online={true}
-            username={onlinePeopleExclOurUser[userId]}
-            onClick={() => setSelectedUserId(userId)}
-            selected={userId === selectedUserId}
-          />
-        ))}
+          {/* display online contacts */}
+          {Object.keys(onlinePeopleExclOurUser).map((userId) => (
+            <Contact
+              key={userId}
+              id={userId}
+              online={true}
+              username={onlinePeopleExclOurUser[userId]}
+              onClick={() => setSelectedUserId(userId)}
+              selected={userId === selectedUserId}
+            />
+          ))}
 
-        {/* display offline contacts */}
-        {Object.keys(offlinePeople).map((userId) => (
-          <Contact
-            key={userId}
-            id={userId}
-            online={false}
-            username={offlinePeople[userId].name} // offlinePeople[userId] is the full object so need to add .username
-            onClick={() => setSelectedUserId(userId)}
-            selected={userId === selectedUserId}
-          />
-        ))}
+          {/* display offline contacts */}
+          {Object.keys(offlinePeople).map((userId) => (
+            <Contact
+              key={userId}
+              id={userId}
+              online={false}
+              username={offlinePeople[userId].name} // offlinePeople[userId] is the full object so need to add .username
+              onClick={() => setSelectedUserId(userId)}
+              selected={userId === selectedUserId}
+            />
+          ))}
+        </div>
       </div>
 
       {/* if no user is selected, display message to select a user*/}
@@ -255,18 +257,20 @@ export default function Messages() {
             {/* upload attachment */}
             <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
               <input type="file" className="hidden" onChange={sendFile} />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <div className="mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
             </label>
 
             {/* send message */}
