34fa8fe569918de98031dc30ec7476fc70e03ff2
shantelletoh <shantelleschool@gmail.com>
2023-04-24 21:38:58 -0700

    add messenger nav to header, update header styles

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -8,14 +8,14 @@ export default function Header() {
   return (
     <header className="flex justify-between">
       {/* flex puts items on same line. justify-bet gives as much space between items as poss */}
-      <Link to={"/"} className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-2">
         <svg // https://heroicons.com/
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 -rotate-90"
+          className="w-7 h-7 -rotate-90"
         >
           <path
             strokeLinecap="round"
@@ -23,19 +23,23 @@ export default function Header() {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">airbnc</span>
+        <span className="font-bold text-2xl">airbnb</span>
       </Link>
-
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
-        <div>Anywhere</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Any week</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Add guests</div>
-      </div>
-      <Link
-        to={user ? "/account" : "/login"}
-        className="
+      <div class="flex items-center gap-3">
+        <Link to={"/messenger"} className="text-gray-500 hover:text-gray-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-8 h-8"
+          >
+            <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
+            <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
+          </svg>
+        </Link>
+        <Link
+          to={user ? "/account" : "/login"}
+          className="
             flex
             items-center
             gap-2
@@ -44,38 +48,40 @@ export default function Header() {
             rounded-full
             py-2
             px-4"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
-          {/* hide overflow to see border */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            fill="none"
             viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6 relative top-1"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
           >
             <path
-              fillRule="evenodd"
-              d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-              clipRule="evenodd"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
-        </div>
-        {!!user && <div>{user.name}</div>} {/* !! converts user to a boolean */}
-      </Link>
+          <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            {/* hide overflow to see border */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 relative top-1"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {!!user && <div>{user.name}</div>}{" "}
+          {/* !! converts user to a boolean */}
+        </Link>
+      </div>
     </header>
   );
 }
